edf19b876a4564242afde432544167961786ff05
Radhika Nyati <rnyati@sapient.com>
2017-04-28 15:02:08 +0530

    Google sign in integrated

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,34 @@
 
-angular.module('starter', ['ngCookies', 'ionic', 'starter.controllers'])
+angular.module('starter', ['ngCookies', 'ionic'])
 
 
-.run(function ($rootScope, $cookieStore, $state) {
+.run(function ($rootScope, $cookieStore, $state, $ionicPlatform) {
+
+     $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
     // Check login session
-    $rootScope.$on('$stateChangeStart', function (event, next, current) {
-        var userInfo = $cookieStore.get('userInfo');
-        if (!userInfo) {
-            // user not logged in | redirect to login
-            if (next.name !== "welcome") {
-                // not going to #welcome, we should redirect now
-                event.preventDefault();
-                $state.go('welcome');
-            }
-        } else if (next.name === "welcome") {
-            event.preventDefault();
-            $state.go('dashboard');
-        }
-    })
+    // $rootScope.$on('$stateChangeStart', function (event, next, current) {
+    //     var userInfo = $cookieStore.get('userInfo');
+    //     if (!userInfo) {
+    //         // user not logged in | redirect to login
+    //         if (next.name !== "welcome") {
+    //             // not going to #welcome, we should redirect now
+    //             event.preventDefault();
+    //             $state.go('welcome');
+    //         }
+    //     } else if (next.name === "welcome") {
+    //         event.preventDefault();
+    //         $state.go('dashboard');
+    //     }
+    // })
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -78,8 +89,7 @@ angular.module('starter', ['ngCookies', 'ionic', 'starter.controllers'])
        url: '/home',
        
         
-           templateUrl: 'templates/home.html',
-           controller: 'LoginController'
+           templateUrl: 'templates/home.html'
          
        
     })
@@ -96,13 +106,14 @@ angular.module('starter', ['ngCookies', 'ionic', 'starter.controllers'])
         controller: "dashboardCtrl"
      })
 
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/welcome');
 })
 
 .controller('ListController', ['$scope', '$http', '$state',
     function($scope, $http, $state) {
     $http.get('http://10.202.249.51:8080/charity').success(function(data) {
       $scope.ngo = data.ngo;
+      console.log('hello');
       console.log(data);
       /*$scope.whichngo=$state.params.ngoName;
       console.log($scope.whichngo);*/
@@ -159,44 +170,159 @@ angular.module('starter', ['ngCookies', 'ionic', 'starter.controllers'])
   });
 }])
 
-.controller('LoginController',function($scope, $state, $ionicLoading) {
-  // This method is executed when the user press the "Sign in with Google" button
+// .controller('LoginController',function($scope, $state, $ionicLoading) {
+//   // This method is executed when the user press the "Sign in with Google" button
 
-  $scope.googleSignIn = function() {
-    console.log('Inside login controller');
-    $ionicLoading.show({
-      template: 'Logging in...'
-    });  
+//   $scope.googleSignIn = function() {
+//     console.log('Inside login controller');
+//     $ionicLoading.show({
+//       template: 'Logging in...'
+//     });  
 
-    window.plugins.googleplus.login(
-      {
-        'webClientId': '374010870485-pq83isui0ccj4t1ts0elb8eo200ond3k.apps.googleusercontent.com',
-        'offline': true
+//     window.plugins.googleplus.login(
+//       {
+//         'webClientId': '374010870485-pq83isui0ccj4t1ts0elb8eo200ond3k.apps.googleusercontent.com',
+//         'offline': true
 
-      },
+//       },
      
-    function (user_data) {
-      console.log(user_data)
-      var data = {
-      userId: user_data.userId,
-      userName: user_data.displayName,
-      userType : 'C'
-      /*email: user_data.email,
-      picture: user_data.imageUrl,
-      accessToken: user_data.accessToken,
-      idToken: user_data.idToken*/
-      };
-      console.log(data);
-      $http.post('http://10.202.249.51:8080/users', data)
+//     function (user_data) {
+//       console.log(user_data)
+//       var data = {
+//       userId: user_data.userId,
+//       userName: user_data.displayName,
+//       userType : 'C'
+//       /*email: user_data.email,
+//       picture: user_data.imageUrl,
+//       accessToken: user_data.accessToken,
+//       idToken: user_data.idToken*/
+//       };
+//       console.log(data);
+//       $http.post('http://10.202.249.51:8080/users', data)
 
-      $ionicLoading.hide();
-      $state.go('home');
-      },
-      function (msg) {
-      $ionicLoading.hide();
-      }
-    );
-  };
+//       $ionicLoading.hide();
+//       $state.go('home');
+//       },
+//       function (msg) {
+//       $ionicLoading.hide();
+//       }
+//     );
+//   };
+// })
+
+
+
+.controller('welcomeCtrl', function ($scope, $state, $cookieStore) {
+
+    /**
+     * SOCIAL LOGIN
+     * Facebook and Google
+     */
+    // FB Login
+    $scope.fbLogin = function () {
+        FB.login(function (response) {
+            if (response.authResponse) {
+                getUserInfo();
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }, {scope: 'email,user_photos,user_videos'});
+
+        function getUserInfo() {
+            // get basic info
+            FB.api('/me', function (response) {
+                console.log('Facebook Login RESPONSE: ' + angular.toJson(response));
+                // get profile picture
+                FB.api('/me/picture?type=normal', function (picResponse) {
+                    console.log('Facebook Login RESPONSE: ' + picResponse.data.url);
+                    response.imageUrl = picResponse.data.url;
+                    // store data to DB - Call to API
+                    // Todo
+                    // After posting user data to server successfully store user data locally
+                    var user = {};
+                    user.name = response.name;
+                    user.email = response.email;
+                    if(response.gender) {
+                        response.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
+                    } else {
+                        user.gender = '';
+                    }
+                    user.profilePic = picResponse.data.url;
+                    $cookieStore.put('userInfo', user);
+                    $state.go('dashboard');
+
+                });
+            });
+        }
+    };
+    // END FB Login
+
+    // Google Plus Login
+    $scope.gplusLogin = function () {
+        var myParams = {
+            // Replace client id with yours
+            'clientid': '374010870485-pq83isui0ccj4t1ts0elb8eo200ond3k.apps.googleusercontent.com',
+            'cookiepolicy': 'single_host_origin',
+            'callback': loginCallback,
+            'approvalprompt': 'force',
+            'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+        };
+        gapi.auth.signIn(myParams);
+
+        function loginCallback(result) {
+            if (result['status']['signed_in']) {
+                var request = gapi.client.plus.people.get({'userId': 'me'});
+                request.execute(function (resp) {
+                    console.log('Google+ Login RESPONSE: ' + angular.toJson(resp));
+                    var userEmail;
+                    if (resp['emails']) {
+                        for (var i = 0; i < resp['emails'].length; i++) {
+                            if (resp['emails'][i]['type'] == 'account') {
+                                userEmail = resp['emails'][i]['value'];
+                            }
+                        }
+                    }
+                    // store data to DB
+                    var user = {};
+                    user.name = resp.displayName;
+                    user.email = userEmail;
+                    if(resp.gender) {
+                        resp.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
+                    } else {
+                        user.gender = '';
+                    }
+                    user.profilePic = resp.image.url;
+                    $cookieStore.put('userInfo', user);
+                    $state.go('dashboard');
+                });
+            }
+        }
+    };
+    // END Google Plus Login
+
+})
+
+// Dashboard/Profile Controller
+.controller('dashboardCtrl', function ($scope, $window, $state, $cookieStore) {
+    // Set user details
+    $scope.user = $cookieStore.get('userInfo');
+
+    
+    // Logout user
+    $scope.logout = function () {
+      console.log('In logout');
+
+       // document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:8100"; 
+        $cookieStore.remove("userInfo");
+        console.log("Hello");
+        console.log('userInfo');
+        $state.go('welcome');
+        $window.location.reload();
+        
+
+
+
+    };
 });
 /*.controller('LoginController',function($scope, $state, UserService, $ionicLoading) {
   // This method is executed when the user press the "Sign in with Google" button
