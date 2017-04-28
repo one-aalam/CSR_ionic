711f4b1e79d2dfae04ac36e12752c906c7f46325
Radhika Nyati <rnyati@sapient.com>
2017-04-28 12:17:28 +0530

    Google api

--- www/js/app.js
+++ www/js/app.js
@@ -1,52 +1,24 @@
 
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ngCookies', 'ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+
+.run(function ($rootScope, $cookieStore, $state) {
+    // Check login session
+    $rootScope.$on('$stateChangeStart', function (event, next, current) {
+        var userInfo = $cookieStore.get('userInfo');
+        if (!userInfo) {
+            // user not logged in | redirect to login
+            if (next.name !== "welcome") {
+                // not going to #welcome, we should redirect now
+                event.preventDefault();
+                $state.go('welcome');
+            }
+        } else if (next.name === "welcome") {
+            event.preventDefault();
+            $state.go('dashboard');
+        }
+    })
 })
-
-/*.run(["$ionicPlatform", "$window", function($ionicPlatform,$window) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.plugins && window.plugins.googleplus) {
-            window.plugins.googleplus.isAvailable(
-                function (available) {
-                if (available) {
-                  $window.plugins.googleplus.show(true);
-                    // show the Google+ sign-in button
-                    console.log('available')
-                  }
-                  else{
-                    console.log('not available')
-                }
-            });
-        }
-        else{
-          console.log('window.plugins.googleplus not found')
-        }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-}])*/
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -106,12 +78,25 @@ angular.module('starter', ['ionic'])
        url: '/home',
        
         
-           templateUrl: 'templates/home.html'
+           templateUrl: 'templates/home.html',
+           controller: 'LoginController'
          
        
     })
 
-  $urlRouterProvider.otherwise('/list');
+     .state('welcome', {
+        url: "/welcome",
+        templateUrl: "templates/welcome.html",
+        controller: 'welcomeCtrl'
+        })
+
+     .state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "templates/dashboard.html",
+        controller: "dashboardCtrl"
+     })
+
+  $urlRouterProvider.otherwise('/home');
 })
 
 .controller('ListController', ['$scope', '$http', '$state',
@@ -169,14 +154,16 @@ angular.module('starter', ['ionic'])
     $scope.charity = data;
     $scope.event = data.charityEvent;
     $scope.requirement = event.requirement;
-  
+
     console.log(event);
   });
-}]);
+}])
 
-/*.controller('LoginController',function($scope, $state, UserService, $ionicLoading) {
+.controller('LoginController',function($scope, $state, $ionicLoading) {
   // This method is executed when the user press the "Sign in with Google" button
+
   $scope.googleSignIn = function() {
+    console.log('Inside login controller');
     $ionicLoading.show({
       template: 'Logging in...'
     });  
@@ -187,6 +174,41 @@ angular.module('starter', ['ionic'])
         'offline': true
 
       },
+     
+    function (user_data) {
+      console.log(user_data)
+      var data = {
+      userId: user_data.userId,
+      userName: user_data.displayName,
+      userType : 'C'
+      /*email: user_data.email,
+      picture: user_data.imageUrl,
+      accessToken: user_data.accessToken,
+      idToken: user_data.idToken*/
+      };
+      console.log(data);
+      $http.post('http://10.202.249.51:8080/users', data)
+
+      $ionicLoading.hide();
+      $state.go('home');
+      },
+      function (msg) {
+      $ionicLoading.hide();
+      }
+    );
+  };
+});
+/*.controller('LoginController',function($scope, $state, UserService, $ionicLoading) {
+  // This method is executed when the user press the "Sign in with Google" button
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });  
+    window.plugins.googleplus.login(
+      {
+        'webClientId': '374010870485-pq83isui0ccj4t1ts0elb8eo200ond3k.apps.googleusercontent.com',
+        'offline': true
+      },
       function (user_data) {
         UserService.setUser({
           userID: user_data.userId,
@@ -196,9 +218,7 @@ angular.module('starter', ['ionic'])
           accessToken: user_data.accessToken,
           idToken: user_data.idToken
           
-
         });
-
         $ionicLoading.hide();
         $state.go('tabs.list');
       },
@@ -207,9 +227,10 @@ angular.module('starter', ['ionic'])
       }
     );
   };
-})*/
+})
 
-/*.service('UserService', function() {
+
+.service('UserService', function() {
   // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
 
   var setUser = function(user_data) {
